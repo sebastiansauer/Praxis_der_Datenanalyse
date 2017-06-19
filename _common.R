9251b524dfd53c8bbbd9441ffc9f02e2e2077040
Sebastian Sauer <ssauer@posteo.de>
2017-06-19 13:46:39 +0200

    parametrized reports, add _common.r

--- _common.R
+++ _common.R
@@ -3,17 +3,19 @@ options(digits = 3)
 
 knitr::opts_chunk$set(
   comment = "#>",
-  comment = FALSE,
   collapse = TRUE,
-  warning = FALSE,
   message = FALSE,
+  warning = FALSE,
   cache = TRUE,
   out.width = "70%",
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold",
-  tidy.opts = list(width.cutoff=60),
-  tidy=TRUE
+  size = "tiny"
 )
 
+script <- TRUE
+
+
+library(methods)  # sometimes not loaded, although built-in, appears like a bug
