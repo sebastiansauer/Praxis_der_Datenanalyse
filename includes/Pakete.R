db329655549c6f6e91e6a96ad1426ab51b99a3d4
Sebastian Sauer <ssauer@posteo.de>
2017-06-19 21:34:21 +0200

    add includes/

---
+++ includes/Pakete.R
@@ -0,0 +1,5 @@
+source("./includes/my_package_list.R")
+
+Pakete <- sort(unique(c(packages, "tidyverse", "readr", "knitr", "stringr", "car", "nycflights13", "ISLR", "pdftools", "downloader", "ggdendro", "gridExtra", "tm", "tidytext", "lsa", "SnowballC", "wordcloud", "RColorBrewer", "okcupiddata", "reshape2", "wesanderson", "GGally", "titanic", "compute.es", "corrr", "rpart", "rpart.plot", "MASS", "titanic", "arules", "arulesViz", "SDMTools", "corrplot", "gplots", "corrplot", "scatterplot3d", "BaylorEdPsych", "nFactors", "rmarkdown", "methods", "broom", "tidyr", "caret", "methods")))
+
+# lapply(Pakete, require, character.only = TRUE)
